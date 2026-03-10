c2381f66661e88a436c4c4215d05e33d52b56623
ravibrs <boddupallirs@gmail.com>
2026-03-10 12:25:31 -0400

    Added logging to EmployeeServiceImpl

--- src/main/java/com/ems/service/EmployeeServiceImpl.java
+++ src/main/java/com/ems/service/EmployeeServiceImpl.java
@@ -5,9 +5,11 @@ import com.ems.model.Employee;
 import com.ems.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
+
 
 import java.util.List;
-
+@Slf4j
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
@@ -16,25 +18,27 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public List<Employee> getAllEmployees() {
+        log.info("Fetching all employees");
         return repository.findAll();
     }
 
     @Override
     public Employee getEmployeeById(Long id) {
+        log.info("Fetching employee with ID: {}", id);
         return repository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException("Employee not found"));
     }
 
     @Override
     public Employee createEmployee(Employee employee) {
+        log.info("Creating new employee: {}", employee);
         return repository.save(employee);
     }
 
     @Override
     public Employee updateEmployee(Long id, Employee employee) {
-
+        log.info("Updating employee with ID: {}", id);
         Employee existing = getEmployeeById(id);
-
         existing.setFirstName(employee.getFirstName());
         existing.setLastName(employee.getLastName());
         existing.setEmailAddress(employee.getEmailAddress());
@@ -51,7 +55,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public void deleteEmployee(Long id) {
-
+        log.info("Deleting employee with ID: {}", id);
         Employee employee = getEmployeeById(id);
         repository.delete(employee);
     }
